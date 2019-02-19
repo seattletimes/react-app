4600f510aa3dca1e23c98a5d515ef5edc0885946
emilymeng <eng.emilym@gmail.com>
2019-02-18 19:22:45 -0800

    react init

---
+++ my-app/src/index.js
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Flipper, Flipped } from "react-flip-toolkit";
+import styles from "./styles.css";
+
+import photoCaprellids from "./photos/Caprellids.jpg";
+import photoChinook from "./photos/Chinook.jpg";
+import photoHerring from "./photos/Herring.jpg";
+import photoPompano from "./photos/Pompano.jpg";
+import photoPryosomes from "./photos/Pryosomes.jpg";
+import photoSeaNettle from "./photos/SeaNettle.jpg";
+import photoSquid from "./photos/Squid.jpg";
+import photoTunicat from "./photos/Tunicat.jpg";
+import photoWolf_Eel from "./photos/Wolf_Eel.jpg";
+
+class ListAll extends Component {
+  state = { data: [photoCaprellids, photoChinook, photoHerring, photoPompano, photoPryosomes, photoSeaNettle, photoSquid, photoTunicat, photoWolf_Eel] };
+	
+	allItems = () => {
+    var filter = [photoCaprellids, photoChinook, photoHerring, photoPompano, photoPryosomes, photoSeaNettle, photoSquid, photoTunicat, photoWolf_Eel];
+    this.setState({ data: filter });
+  };
+
+  expectedItems = () => {
+    var filter = [photoCaprellids, photoChinook, photoHerring, photoSeaNettle, photoTunicat, photoWolf_Eel];
+    this.setState({ data: filter });
+  };
+
+  unexpectedItems = () => {
+    var filter = [photoPompano, photoPryosomes, photoSquid];
+    this.setState({ data: filter });
+  };
+
+  render() {
+    return (
+      <Flipper flipKey={this.state.data.join("www")}>
+        <button onClick={this.allItems}> All organisms</button>
+        <button onClick={() => this.expectedItems()}>Expected organisms</button>
+        <button onClick={() => this.unexpectedItems()}>Unexpected organisms</button>
+        <ul className="list">
+          {this.state.data.map(d => (
+            <Flipped key={d} flipId={d}>
+              <li>
+                <img src= {d} />
+              </li>
+            </Flipped>
+          ))}
+        </ul>
+      </Flipper>
+    );
+  }
+}
+
+ReactDOM.render(<ListAll />, document.querySelector("#root"));
+
+
+// const Square = ({ toggleFullScreen }) => (
+//   <Flipped flipId="square">
+//     <div className="square" onClick={toggleFullScreen} />
+//   </Flipped>
+// );
+
+// const FullScreenSquare = ({ toggleFullScreen }) => (
+//   <Flipped flipId="square">
+//     <div className="full-screen-square" onClick={toggleFullScreen} />
+//   </Flipped>
+// );
+
+// class AnimatedSquare extends Component {
+//   state = { fullScreen: false };
+
+//   toggleFullScreen = () => {
+//     this.setState(prevState => ({
+//       fullScreen: !prevState.fullScreen
+//     }));
+//   };
+
+//   render() {
+//     return (
+//       <Flipper flipKey={this.state.fullScreen}>
+//         {this.state.fullScreen ? (
+//           <FullScreenSquare toggleFullScreen={this.toggleFullScreen} />
+//         ) : (
+//           <Square toggleFullScreen={this.toggleFullScreen} />
+//         )}
+//       </Flipper>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<AnimatedSquare />, document.querySelector("#root"));
