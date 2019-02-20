ca969fbd23cde7173754e42069d91e2c9e205668
emilymeng <eng.emilym@gmail.com>
2019-02-20 02:02:03 -0800

    magic

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -14,45 +14,88 @@ import photoSquid from "./photos/Squid.jpg";
 import photoTunicat from "./photos/Tunicat.jpg";
 import photoWolf_Eel from "./photos/Wolf_Eel.jpg";
 
+const Fish = ({ toggleFullScreen, image }) => (
+  <div>
+    <img className="square image" src={image} onClick={toggleFullScreen} />
+  </div>
+);
 
+const FullScreenFish = ({ toggleFullScreen, image, text }) => (
+  <div className="full-screen-square image">
+    <img src={image} onClick={toggleFullScreen} />
+    <h2>{text}</h2>
+  </div>
+);
 
+class AnimatedFish extends Component {
+  state = { fullScreen: false };
+
+  toggleFullScreen = () => {
+    this.setState(prevState => ({
+      fullScreen: !prevState.fullScreen
+    }));
+  };
+
+  render() {
+    return (
+        <Flipped key={this.props.d} flipId={this.props.flipId}>
+            <li>
+                {this.state.fullScreen ? (
+          <FullScreenFish toggleFullScreen={this.toggleFullScreen} image={this.props.image} text={this.props.text} />
+        ) : (
+          <Fish toggleFullScreen={this.toggleFullScreen} image={this.props.image} text={this.props.text} />
+        )}
+            </li>
+        </Flipped>
+    );
+  }
+}
+
+var chinook = {
+  image: photoChinook,
+  text: "I am a Chinook RAWRRRRR"
+};
 
 class ListAll extends Component {
-  state = { data: [photoCaprellids, photoChinook, photoHerring, photoPompano, photoPryosomes, photoSeaNettle, photoSquid, photoTunicat, photoWolf_Eel] };
+  // state = { data: [photoChinook, photoCaprellids, photoPryosomes, photoSeaNettle, photoPompano, photoWolf_Eel, photoSquid, photoTunicat, photoHerring] };
+  state = { data: [chinook] };
 	
 	allItems = () => {
-    var filter = [photoCaprellids, photoChinook, photoHerring, photoPompano, photoPryosomes, photoSeaNettle, photoSquid, photoTunicat, photoWolf_Eel];
+    // var filter = [photoChinook, photoCaprellids, photoPryosomes, photoSeaNettle, photoPompano, photoWolf_Eel, photoSquid, photoTunicat, photoHerring];
+    var filter = [chinook];
     this.setState({ data: filter });
   };
 
   expectedItems = () => {
-    var filter = [photoCaprellids, photoChinook, photoHerring, photoSeaNettle, photoTunicat, photoWolf_Eel];
+    // var filter = [photoChinook, photoCaprellids, photoSeaNettle, photoWolf_Eel, photoTunicat, photoHerring];
+    var filter = [chinook];
     this.setState({ data: filter });
   };
 
   unexpectedItems = () => {
-    var filter = [photoPompano, photoPryosomes, photoSquid];
+    // var filter = [photoPryosomes, photoPompano, photoSquid];
+    var filter = [chinook];
     this.setState({ data: filter });
   };
 
   render() {
     return (  
-      <Flipper flipKey={this.state.data.fullScreen}>
+      <Flipper flipKey={this.state.data}>
+      <div className="groupButton">
         <button onClick={this.allItems}> All organisms</button>
         <button onClick={() => this.expectedItems()}>Expected organisms</button>
         <button onClick={() => this.unexpectedItems()}>Unexpected organisms</button>
+      </div>
         <ul className="list">
           {this.state.data.map(d => (
-            <Flipped key={d} flipId={d}>
-              <li>
-                <img className= "image" src= {d} />
-              </li>
-            </Flipped>
+            <AnimatedFish key={d} flipId={d} image={d.image} text={d.text}/>
           ))}
         </ul>
       </Flipper>
+
     );
   }
 }
+
 
 ReactDOM.render(<ListAll />, document.querySelector("#root"));
